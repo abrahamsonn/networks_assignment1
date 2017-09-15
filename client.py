27f8b6d12685a7bacfa68163656b604b987c8875
abrahamsonn <alex@wolfssl.com>
2017-09-14 19:49:15 -0600

    client.py update

--- client.py
+++ client.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python
-import argparse
+import sys
 import httplib
+import urllib
 
 def main():
 
+    if len(sys.argv) != 5:
+        print 'Enter the correct args (IP, port, X, Y)'
+        return 0
+ 
+    ip = sys.argv[1]
+    port = sys.argv[2]
+    X = sys.argv[3]
+    Y = sys.argv[4]
+
+    params = urllib.urlencode({"X": X, "Y": Y})
+    headers = {"Accept": "text/plain", "Accept-Language": "en-us,en;"}
+    connection = httplib.HTTPConnection(ip, port)
+    connection.request("POST", "", params, headers)
+    response = connection.getresponse()
+
+    print response.status, response.reason
+
+    data = response.read()
+    print data
 
 if __name__ == '__main__':
     main()
+
