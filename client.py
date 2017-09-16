5dd01e9408ef7e3ca24c5773e0a36523128e943c
abrahamsonn <alex@wolfssl.com>
2017-09-16 10:54:10 -0600

    It's working better than it was - server is actually receiving BODY, params, and headers now

--- client.py
+++ client.py
@@ -7,6 +7,7 @@ import urllib
 
 def main():
 
+# Argument checking
     if len(sys.argv) != 5:
         print 'Enter the correct args (IP, port, X, Y)'
         return 0
@@ -16,16 +17,33 @@ def main():
     X = sys.argv[3]
     Y = sys.argv[4]
 
-    params = urllib.urlencode({"X": X, "Y": Y})
+# Format the POST request that will be sent to the server & send it
+    params = urllib.urlencode({'X': X, 'Y': Y})
     headers = {"Accept": "text/plain", "Accept-Language": "en-us,en;"}
-    connection = httplib.HTTPConnection(ip, port)
-    connection.request("POST", str(X)+","+str(Y), params, headers)
+    connection = httplib.HTTPConnection(str(ip), port)
+    connection.request("POST", "FIRE", params, headers)
+
+# Read server's response and print to user
     response = connection.getresponse()
 
-    print response.status, response.reason
-
     data = response.read()
+    print '-------------------- Data --------------------'
     print data
+    print '----------------------------------------------'
+
+    print
+
+    print '-------------- response.status ---------------'
+    print response.status 
+    print '----------------------------------------------'
+
+    print
+
+    print '-------------- response.reason ---------------'
+    print response.reason # OK
+    print '----------------------------------------------'
+
+    print
 
     connection.close()
 
--- server.py
+++ server.py
@@ -1,34 +1,55 @@
+# server.py
+
 #!/usr/bin/env python
 import sys
 import httplib
+import http.server
 import urllib
 import socket
 
 def main():
    
-    if len(sys.argv) != 4:
-        print 'Enter the correct args (IP, port, board.txt)'
+# Argument checking
+    if len(sys.argv) != 3:
+        print 'Enter the correct args (port, board.txt)'
         return 0
 
-    ip = sys.argv[1]
-    port = sys.argv[2]
-    board = sys.argv[3]
+    port = sys.argv[1]
+    board = sys.argv[2]
 
+# Create and bind the socket for the client to connect to, and then wait
     socketfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     socketfd.bind((ip, int(port)))
-#    socketfd.bind(('127.0.0.1', 11111))
     socketfd.listen(1)
-
-#   board_array = [10][10]
 
 #   board_file = open(board, 'wb+')
 #   for y in board_file:
 #       for x in board_file:
 #           board[y][x] = board_file.read()
 
+# Receive data from the client in the form of an HTTP POST request
     connection, address = socketfd.accept()
-    data = connection.recv(50)
+    data = connection.recv(4096)
+    print '-------------------- Data --------------------'
     print data
+    print '----------------------------------------------'
+    print 
+
+    print '---------------- Coordinates: ----------------'
+    print 
+    print '----------------------------------------------'
+    print
+# Reply as HTTP/1.1 server, saying "HTTP OK" (code 200).
+    A = 'HTTP/1.1'
+    B = '200'
+    C = 'OK'        
+    connection.send('%s %s %s' % (A, B, C))
+
+# Respond with a hit or miss
+    print '------------------ Sending: ------------------'
+    print data
+    print '----------------------------------------------'
+    print 
     connection.send(data)
     connection.close()
 
