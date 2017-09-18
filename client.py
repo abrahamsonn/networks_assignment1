f69b8f073c4f88a5400f4fd6429162cd94555882
abrahamsonn <alex@wolfssl.com>
2017-09-18 16:47:00 -0600

    Updated socket-server.py

--- client.py
+++ client.py
@@ -4,48 +4,91 @@
 import sys
 import httplib
 import urllib
+import re
 
 def main():
 
-# Argument checking
+    # Argument checking
     if len(sys.argv) != 5:
         print 'Enter the correct args (IP, port, X, Y)'
         return 0
- 
+
     ip = sys.argv[1]
     port = sys.argv[2]
     X = sys.argv[3]
     Y = sys.argv[4]
 
-# Format the POST request that will be sent to the server & send it
+    # Format the POST request that will be sent to the server & send it
     params = urllib.urlencode({'X': X, 'Y': Y})
     headers = {"Accept": "text/plain", "Accept-Language": "en-us,en;"}
     connection = httplib.HTTPConnection(str(ip), port)
     connection.request("POST", "FIRE", params, headers)
 
-# Read server's response and print to user
+    # Read server's response and print to user
     response = connection.getresponse()
 
     data = response.read()
-    print '-------------------- Data --------------------'
+
     print data
-    print '----------------------------------------------'
 
-    print
+    hitRegex = re.compile('hit=\d')
+    hitResponse = re.search(hitRegex, data).group()
 
-    print '-------------- response.status ---------------'
-    print response.status 
-    print '----------------------------------------------'
+    if hitResponse == 'None':
+        print 'No hit data was received'
+        return 0
+    else:
+        isHit = hitResponse[4]
 
-    print
+    UpdateKnowledgeBoard(isHit, Y, X)
 
-    print '-------------- response.reason ---------------'
-    print response.reason # OK
-    print '----------------------------------------------'
-
-    print
+    return 0
 
     connection.close()
+
+def InitiateKnowledgeBoard():
+    knowledgeBoardFile = open('knowledgeBoard.txt', 'w')
+    for i in range(1,11):
+        knowledgeBoardFile.write('__________\n')
+
+def UpdateKnowledgeBoard(isHit, inY, inX):
+    x = int(inX)
+    y = int(inY)
+
+    try:
+        print 'try this'
+        knowledgeBoardFile = open('knowledgeBoard.txt', 'r')
+    except:
+        InitiateKnowledgeBoard()
+        knowledgeBoardFile = open('knowledgeBoard.txt', 'r')
+    knowledgeBoard = [line for line in knowledgeBoardFile]
+
+    newKnowledgeBoard = []
+
+    for oldLine in range(0,10):
+        newKnowledgeBoard.append([])
+        for oldColumn in range(0,10):
+            newKnowledgeBoard[oldLine].append(ord(knowledgeBoard[oldLine][oldColumn]))
+
+    if isHit == '1':
+        newKnowledgeBoard[y][x] = 88
+    elif isHit == '0':
+        newKnowledgeBoard[y][x] = 79
+    else:
+        return 1
+
+    knowledgeBoardFile.close()
+
+    newKnowledgeBoardFile = open('knowledgeBoard.txt', 'w')
+
+    for newLine in range(0,10):
+        for newColumn in range(0,10):
+            newCharacter = chr(newKnowledgeBoard[newLine][newColumn])
+            newKnowledgeBoardFile.write(newCharacter)
+        newKnowledgeBoardFile.write('\n')
+    knowledgeBoardFile.close()
+
+    return 0
 
 if __name__ == '__main__':
     main()
--- socket-server.py
+++ socket-server.py
@@ -65,88 +65,111 @@ def main():
             else:
                 parsed_data + ' '
 
-    # Search parsed_data for the X=#&Y=# pattern
-        Y = 10
-        X = 10
-        pattern = re.compile('Y=\d&X=\d')
-        found = re.search(pattern, parsed_data).group()
+        if re.search('POST FIRE', parsed_data) != None:
+        # Search parsed_data for the X=#&Y=# pattern
+            Y = 10
+            X = 10
+            pattern = re.compile('Y=\d+&X=\d+')
+            found = re.search(pattern, parsed_data).group()
 
-    # Check to see that the position is valid, and if it is, fire
-        sink = 'F'
-        hit = 0
+        # Check to see that the position is valid, and if it is, fire
+            sink = 'F'
+            hit = 0
 
-        if found == 'None': # No coordinates in the message
-            print 'No X,Y coordinates were passed. Try again.'
-            connectionSocket.send('Your X,Y coordinates did not arrive/were not found')
-            connectionSocket.close()
-            continue
-        else:
-            Y = int(found[2])
-            X = int(found[6])
-
-            if (X > 9 or Y > 9) or (X < 0 or Y < 0): # Out-of-bounds coordinates
-                connectionSocket.send('X,Y coordinates out-of-bounds')
-                connectionSocket.close()
-                continue
-            elif board[Y][X] == 'X' or board[Y][X] == '+': # Position already fired upon
-                connectionSocket.send('You\'ve already fired upon that position...')
+            if found == 'None': # No coordinates in the message
+                print 'No X,Y coordinates were passed. Try again.'
+                connectionSocket.send('Your X,Y coordinates did not arrive/were not found')
                 connectionSocket.close()
                 continue
             else:
-            # If it's a valid position, determine if it's a hit or miss
-                print "Salvo fired upon " + "[" + str(Y) + "][" + str(X) + "]\n"
-                if board[Y][X] == '_':
-                    print 'Miss!'
-                    # How do you replace single elements of an array in python?
-                    board[Y][X].replace(board[Y][X], "+")
+                line = re.split('&', found)
+                print line
+                y = re.split('=', line[0])
+                Y = int(y[1])
+                x = re.split('=', line[1])
+                X = int(x[1])
+
+                print Y
+                print X
+
+                if (X > 9 or Y > 9) or (X < 0 or Y < 0): # Out-of-bounds coordinates
+                    print 'X,Y coordinates out-of-bounds'
+                    connectionSocket.send('X,Y coordinates out-of-bounds')
+                    connectionSocket.close()
+                    continue
+                elif board[Y][X] == 'X' or board[Y][X] == 'O': # Position already fired upon
+                    print 'You\'ve already fired upon that position...'
+                    connectionSocket.send('You\'ve already fired upon that position...')
+                    connectionSocket.close()
+                    continue
                 else:
-                    hit = 1
-                    print board[Y][X] + " was hit!\n"
-                    # How do you replace single elements of an array in python?
-                    board[Y][X].replace(board[Y][X], "X")
+                # If it's a valid position, determine if it's a hit or miss
+                    print "Salvo fired upon " + "[" + str(Y) + "][" + str(X) + "]\n"
+                    if board[Y][X] == '_':
+                        print 'Miss!'
+                        # How do you replace single elements of an array in python?
+                        board[Y][X].replace(board[Y][X], "O")
+                    else:
+                        hit = 1
+                        print board[Y][X] + " was hit!\n"
+                        # How do you replace single elements of an array in python?
+                        board[Y][X].replace(board[Y][X], "X")
 
-                    # This keeps track of what has/hasn't been sunk
-                    if board[Y][X] == 'C':
-                        C_health = C_health - 1
-                        print 'C_health = ' + str(C_health) + "\n"
-                        if C_health == 0:
-                            print 'C has been sunk!!!'
-                    elif board[Y][X] == 'B':
-                        B_health = B_health - 1
-                        print 'B_health = ' + str(B_health) + "\n"
-                        if B_health == 0:
-                            print 'B has been sunk!!!'
-                    elif board[Y][X] == 'R':
-                        R_health = R_health - 1
-                        print 'R_health = ' + str(R_health) + "\n"
-                        if R_health == 0:
-                            print 'R has been sunk!!!'
-                    elif board[Y][X] == 'S':
-                        S_health = S_health - 1
-                        print 'S_health = ' + str(S_health) + "\n"
-                        if S_health == 0:
-                            print 'S has been sunk!!!'
-                    elif board[Y][X] == 'D':
-                        D_health = D_health - 1
-                        print 'D_health = ' + str(D_health) + "\n"
-                        if D_health == 0:
-                            print 'D has been sunk!!!'
+                        # This keeps track of what has/hasn't been sunk
+                        if board[Y][X] == 'C':
+                            C_health = C_health - 1
+                            print 'C_health = ' + str(C_health) + "\n"
+                            if C_health == 0:
+                                print 'C has been sunk!!!'
+                        elif board[Y][X] == 'B':
+                            B_health = B_health - 1
+                            print 'B_health = ' + str(B_health) + "\n"
+                            if B_health == 0:
+                                print 'B has been sunk!!!'
+                        elif board[Y][X] == 'R':
+                            R_health = R_health - 1
+                            print 'R_health = ' + str(R_health) + "\n"
+                            if R_health == 0:
+                                print 'R has been sunk!!!'
+                        elif board[Y][X] == 'S':
+                            S_health = S_health - 1
+                            print 'S_health = ' + str(S_health) + "\n"
+                            if S_health == 0:
+                                print 'S has been sunk!!!'
+                        elif board[Y][X] == 'D':
+                            D_health = D_health - 1
+                            print 'D_health = ' + str(D_health) + "\n"
+                            if D_health == 0:
+                                print 'D has been sunk!!!'
 
-    # Print the current status of the board
-        print 
-        print '------------------- board: -------------------'
-        for i in board:
-            for j in i:
-                sys.stdout.write(j)
-        print '----------------------------------------------'
-        print 
+        # Print the current status of the board
+            print 
+            print '------------------- board: -------------------'
+            for i in board:
+                for j in i:
+                    sys.stdout.write(j)
+            print '----------------------------------------------'
+            print 
 
-    # Reply to the client & close connection
-        message = 'HTTP /1.1 OK\r\nConnection: Close\r\n\r\nhit=' + str(hit)
-        if sink != 'F':
-            message = message + '&sink=' + str(sink)
-        connectionSocket.send(message)
-        connectionSocket.close()
+        # Reply to the client & close connection
+            message = 'HTTP /1.1 OK\r\nConnection: Close\r\n\r\nhit=' + str(hit)
+            if sink != 'F':
+                message = message + '&sink=' + str(sink)
+            connectionSocket.send(message)
+            connectionSocket.close()
+
+        elif re.search('GET', parsed_data):
+            message = '<!DOCTYPE html><html><body>'
+#            for line in board:
+#                message = message + str(board)
+            for i in board:
+                if i != '\n':
+                    message = message + i
+                elif i == '\n' or i == '\r':
+                    message = message + '<br>'
+            message = message + '</body></html>'
+            connectionSocket.send(str(message))
+            connectionSocket.close()
 
     file_o.close()
 
